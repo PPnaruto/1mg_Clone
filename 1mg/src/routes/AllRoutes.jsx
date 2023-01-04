dbf33298e44c1ec6649481c72e2f1dbc689c7ae7
Alisha Sahu <alishsahu666@gmail.com>
2023-01-04 20:27:57 +0530

    checkout

--- 1mg/src/routes/AllRoutes.jsx
+++ 1mg/src/routes/AllRoutes.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
+import Checkout from "../components/Checkout/checkout";
 import Home from "./Home";
 
 const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/checkout" element={<Checkout/>} />
     </Routes>
   );
 };
