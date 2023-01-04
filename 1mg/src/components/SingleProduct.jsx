555bb468488ab4064793b80b5a75098b28bc800c
PPnaruto <prtaik.35.patil@gmail.com>
2023-01-04 21:02:37 +0530

    Testing Merging Conflict

---
+++ 1mg/src/components/SingleProduct.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+ const SingleProduct = () => {
+  return (
+    <div> <h1>SingleProduct</h1>
+        <input type="text" placeholder='Enter product name' />
+        <button>Submit</button>
+    </div>
+  )
+}
+
+export default SingleProduct;
--- 1mg/src/routes/AllRoutes.jsx
+++ 1mg/src/routes/AllRoutes.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
+import SingleProduct from "../components/SingleProduct";
 import Home from "./Home";
 
 const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/singleproduct" element={<SingleProduct/>}/>
     </Routes>
   );
 };
