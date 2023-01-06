3e5c067bd68fb28c2f1f83b8d5bcf1e6e5470cbd
Vishal Dinesh <vishal.gxm@gmail.com>
2023-01-06 21:42:10 +0530

    vitamins page complete

--- 1mg/src/routes/Vitamins.jsx
+++ 1mg/src/routes/Vitamins.jsx
@@ -1,13 +1,24 @@
-import AllFilters from "../components/AllFilters";
-import ProductsWrapper from "../components/ProductsWrapper";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+import AllFilters from "../components/Product/AllFilters";
+import ProductsWrapper from "../components/Product/ProductsWrapper";
+import fetchData from "../redux/Product/action";
+
 import styles from "../styles/Vitamins.module.css";
 
 const Vitamins = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchData("Vitamins"));
+  }, []);
+
   return (
     <main>
       <section className={styles.product}>
         <div className={styles.filterWrapper}>
-          <AllFilters category="Vitamins" />
+          <AllFilters />
         </div>
         <div className={styles.productWrapper}>
           <ProductsWrapper category="Vitamins" />
@@ -16,5 +27,4 @@ const Vitamins = () => {
     </main>
   );
 };
-
 export default Vitamins;
