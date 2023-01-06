c8e2cce5177eed5a25dc14d21819a5ff0491a51e
Vishal Dinesh <vishal.gxm@gmail.com>
2023-01-06 21:46:48 +0530

    made ayurveda product page

---
+++ 1mg/src/routes/Ayurveda.jsx
@@ -0,0 +1,30 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+import AllFilters from "../components/Product/AllFilters";
+import ProductsWrapper from "../components/Product/ProductsWrapper";
+import fetchData from "../redux/Product/action";
+
+import styles from "../styles/Vitamins.module.css";
+
+const Ayurveda = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchData("Ayurveda"));
+  }, []);
+
+  return (
+    <main>
+      <section className={styles.product}>
+        <div className={styles.filterWrapper}>
+          <AllFilters />
+        </div>
+        <div className={styles.productWrapper}>
+          <ProductsWrapper category="Ayurveda" />
+        </div>
+      </section>
+    </main>
+  );
+};
+export default Ayurveda;
