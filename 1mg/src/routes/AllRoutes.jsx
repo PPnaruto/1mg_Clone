ae75a6b57d0e5189b7c96040bf554cb0278c9d6b
Vishal Dinesh <vishal.gxm@gmail.com>
2023-01-06 21:47:14 +0530

    added routes to individual product pages

--- 1mg/src/routes/AllRoutes.jsx
+++ 1mg/src/routes/AllRoutes.jsx
@@ -2,12 +2,22 @@ import { Routes, Route } from "react-router-dom";
 import Checkout from "../components/Checkout/checkout";
 import Home from "./Home";
 import Vitamins from "./Vitamins";
+import Diabetes from "./Diabetes";
+import Healthcare from "./Healthcare";
+import Personal from "./Personal";
+import Ayurveda from "./Ayurveda";
+import Homeopathy from "./Homeopathy";
 
 const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/products/vitamins" element={<Vitamins />} />
+      <Route path="/products/diabetes" element={<Diabetes />} />
+      <Route path="/products/healthcare" element={<Healthcare />} />
+      <Route path="/products/personal" element={<Personal />} />
+      <Route path="/products/ayurveda" element={<Ayurveda />} />
+      <Route path="/products/homeopathy" element={<Homeopathy />} />
       <Route path="/checkout" element={<Checkout />} />
     </Routes>
   );
