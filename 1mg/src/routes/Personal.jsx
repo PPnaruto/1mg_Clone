7adb57ff53f61de39125422468d2ea06d8300459
Vishal Dinesh <vishal.gxm@gmail.com>
2023-01-06 21:44:19 +0530

    made personal product page

---
+++ 1mg/src/routes/Personal.jsx
@@ -0,0 +1,30 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+import AllFilters from "../components/Product/AllFilters";
+import ProductsWrapper from "../components/Product/ProductsWrapper";
+import fetchData from "../redux/Product/action";
+
+import styles from "../styles/Vitamins.module.css";
+
+const Personal = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchData("Personal"));
+  }, []);
+
+  return (
+    <main>
+      <section className={styles.product}>
+        <div className={styles.filterWrapper}>
+          <AllFilters />
+        </div>
+        <div className={styles.productWrapper}>
+          <ProductsWrapper category="Personal" />
+        </div>
+      </section>
+    </main>
+  );
+};
+export default Personal;
