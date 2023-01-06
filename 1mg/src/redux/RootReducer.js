71d46e5908953bc9338a91b4e1b0f9efc4842d64
Vishal Dinesh <vishal.gxm@gmail.com>
2023-01-06 11:48:17 +0530

    added products reducer

--- 1mg/src/redux/RootReducer.js
+++ 1mg/src/redux/RootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import AuthReducer from "./Auth/AuthReducer";
+import ProductReducer from "./Product/ProductReducer";
 
 const RootReducer = combineReducers({
   auth: AuthReducer,
+  products: ProductReducer,
 });
 
 export default RootReducer;
