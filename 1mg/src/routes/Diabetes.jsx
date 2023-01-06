f1f22ec2d9d39645a84c64d6bff27a84d72a9cc2
Vishal Dinesh <vishal.gxm@gmail.com>
2023-01-06 21:42:39 +0530

    made diabetes page

---
+++ 1mg/src/routes/Diabetes.jsx
@@ -0,0 +1,30 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+import AllFilters from "../components/Product/AllFilters";
+import ProductsWrapper from "../components/Product/ProductsWrapper";
+import fetchData from "../redux/Product/action";
+
+import styles from "../styles/Vitamins.module.css";
+
+const Diabetes = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchData("Diabetes"));
+  }, []);
+
+  return (
+    <main>
+      <section className={styles.product}>
+        <div className={styles.filterWrapper}>
+          <AllFilters />
+        </div>
+        <div className={styles.productWrapper}>
+          <ProductsWrapper category="Diabetes" />
+        </div>
+      </section>
+    </main>
+  );
+};
+export default Diabetes;
