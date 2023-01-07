a5a282587b4bab7ee7cd2249eb10fbaafc967a11
Vishal Dinesh <vishal.gxm@gmail.com>
2023-01-07 19:41:31 +0530

    made product page responsive

--- 1mg/src/App.jsx
+++ 1mg/src/App.jsx
@@ -1,26 +1,10 @@
-
-// import { SignUpLogin } from './components/SignupLogin';
-// import {useState, useEffect} from 'react' ;
+import AllRoutes from "./routes/AllRoutes";
 function App() {
-//  const // [isAuth,setAuth] = useState(false) ;
- 
-//  useEffect(()=>{
-//   if(localStorage.getItem("isAuth")){
-//     setAuth(true) ;
-//    }
-//   }
-//  ,[])
- 
-//  const handleAuth = ()=>{
-//   setAuth(true) ;
-//  }
   return (
-
-  <div></div>
-
-      
+    <div className="App">
+      <AllRoutes />
+    </div>
   );
 }
 
 export default App;
-
