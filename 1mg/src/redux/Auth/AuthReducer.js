43e9d67b3ba04165e693ae159ccade4323bc8b5d
Vishal Dinesh <vishal.gxm@gmail.com>
2023-01-09 00:03:44 +0530

    merged with login page

--- 1mg/src/redux/Auth/AuthReducer.js
+++ 1mg/src/redux/Auth/AuthReducer.js
@@ -1,10 +1,37 @@
-const initial = {
+const initial = JSON.parse(localStorage.getItem("1mg_users")) || {
   isAuth: false,
-  user: {},
+  current: "",
+  users: [],
 };
 
 const AuthReducer = (state = initial, action) => {
   switch (action.type) {
+    case "SIGN_UP":
+      localStorage.setItem(
+        "1mg_users",
+        JSON.stringify({
+          isAuth: true,
+          current: action.payload,
+          users: [...state.users, action.payload],
+        })
+      );
+      return {
+        isAuth: true,
+        current: action.payload,
+        users: [...state.users, action.payload],
+      };
+    case "LOGIN_IN":
+      return {
+        ...state,
+        isAuth: true,
+        current: action.payload,
+      };
+    case "LOG_OUT":
+      return {
+        ...state,
+        isAuth: false,
+        current: "",
+      };
     default:
       return state;
   }
